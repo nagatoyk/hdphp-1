41cf31e9d659f235a04f846ad4f0e050906c0987
向军 <2300071698@qq.com>
2016-08-25 14:23:19 +0800

    1 优化模板标签lists问题 2 修复include 引入模板不能解析变量 3 优化路由解析

--- web/home/controller/Entry.php
+++ web/home/controller/Entry.php
@@ -11,6 +11,14 @@
 class Entry {
 	//首页
 	public function index() {
+		$data = [
+			[ 'name' => '小明', 'age' => 33 ],
+			[ 'name' => '李四', 'age' => 13 ],
+			[ 'name' => '小六', 'age' => 28 ],
+			[ 'name' => '爱华', 'age' => 33 ],
+			[ 'name' => '张三', 'age' => 23 ]
+		];
+		View::with( 'data', $data );
 		View::make();
 	}
 }
--- web/home/view/entry/index.php
+++ web/home/view/entry/index.php
@@ -10,10 +10,33 @@
 <h1 class="text-center text-info" style="margin-top:200px;font-size:80px;">HDPHP 為效率而生</h1>
 <h3 class="text-muted text-center">服务化/组件化/模块化的未来框架产品</h3>
 
-<form action="user/del" method="post">
-	<input type="text" name="user">
-	<input type="hidden" name="_method" value="DELETE">
-	<input type="submit">
-</form>
+<bootstrap/>
+<foreach from="$data" value="$v">
+	{{$v['name']}}
+</foreach>
+<hr>
+<list from='$data' name='$d' row='2' start='1' empty='没有数据'>
+	{{$d['name']}}
+</list>
+<hr>
+<list from='$data' name='$n'>
+	<if value="$hd['list']['n']['first']">
+		{{$hd['list']['n']['index']}}: 这是第一条数据<br/>
+		<elseif value="$hd['list']['n']['last']">
+			{{$hd['list']['n']['index']}}: 最后一条记录<br/>
+			<else>
+				{{$hd['list']['n']['index']}}:{{$n['name']}}<br/>
+	</if>
+</list>
+<hr/>
+共有: {{$hd['list']['n']['total']}} 条记录数<br/>
+是否为第 1 条记录: {{$hd['list']['n']['first']}} <br/>
+是否为最后一条记录: {{$hd['list']['n']['last']}} <br/>
+总记录数: {{$hd['list']['n']['total']}} <br/>
+当前循环是第几条: {{$hd['list']['n']['index']}} <br/>
+<hr>
+<php>if(true){</php>
+后盾网
+<php>}</php>
 </body>
 </html>

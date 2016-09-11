6569adfa45c2eb9ea390ddba94895a316d9e2115
向军 <2300071698@qq.com>
2016-09-12 07:16:00 +0800

    应用

--- system/hdphp.php
+++ system/hdphp.php
@@ -7,9 +7,9 @@
 // |    WeChat: houdunwangxj
 // | Copyright (c) 2012-2019, www.houdunwang.com. All Rights Reserved.
 // '-------------------------------------------------------------------
-/**
- * composer自动加载
- */
+
+//composer自动加载
 require __DIR__ . '/../vendor/autoload.php';
+//启动应用
 $app = new \hdphp\kernel\App();
 $app->bootstrap();
